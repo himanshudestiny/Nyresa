2200272d8cdd76b3430af645ebd337d43940b97c
unknown <suryawanshirahul382@gmail.com>
2022-12-13 21:42:05 +0530

    added

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,16 @@
-
-import './App.css';
-import Products from './components/Products';
+import "./App.css";
+import Products from "./components/Products";
+import AllRoutes from "./pages/AllRoutes";
 
 
 function App() {
   return (
     <div className="App">
+
       
       <Products/>
+      <AllRoutes />
+
       
     </div>
   );
