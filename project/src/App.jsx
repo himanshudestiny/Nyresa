b2f70e67d01d87cf1c8d32ecef0988dccfe7cb45
Yasin051197 <yasinjamadar0511@gmail.com>
2022-12-15 17:51:45 +0530

    added files revert

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,14 +1,50 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Delivery from "./components/Delivery/Delivery";
+import Products from "./components/Products";
+import AllRoutes from "./pages/AllRoutes";
+import { useEffect } from "react";
 
+
+
+
+
+
+
+
+<<<<<<< HEAD:project/src/App.jsx
 import './App.css';
 import ProductDetails from './components/ProductDetails';
 import Products from './components/Products';
+=======
+>>>>>>> b85cab4ff20135667bd64fb0c7c76459e05c4e95:project/src/App.js
 
 function App() {
+
+
+  
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
+<<<<<<< HEAD:project/src/App.jsx
 
       {/* <Products/> */}
       <ProductDetails />
+=======
+      <Navbar />
+      <AllRoutes />
+      <Products />
+
+>>>>>>> b85cab4ff20135667bd64fb0c7c76459e05c4e95:project/src/App.js
     </div>
   );
 }
--- project/src/index.js
+++ project/src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {ChakraProvider} from "@chakra-ui/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
-    <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
