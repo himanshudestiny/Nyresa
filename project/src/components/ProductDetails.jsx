6a262872b85a8bffcff1172b732d9b94421cc526
Yasin051197 <yasinjamadar0511@gmail.com>
2022-12-16 22:12:21 +0530

    added list

--- project/src/components/ProductDetails.jsx
+++ project/src/components/ProductDetails.jsx
@@ -7,6 +7,7 @@ import 'swiper/css';
 import SizeChart from '../image/SizeChart.jpeg'
 import { useNavigate } from 'react-router-dom';
 let count=0;
+let recent_data=JSON.parse(localStorage.getItem("listElement"))||[];
 const ProductDetails = () => {
   const navigate=useNavigate()
   const [flag,setFlag]=useState(false)
@@ -44,14 +45,16 @@ let element=JSON.parse(localStorage.getItem("element"))
     {
       setFlag(true)
       console.log(flag)
+      count++
     }
     else if(count<=1)
     {
       handlechange(e)
     }
   }
-  const handleShopingBag=()=>{
-    localStorage.setItem("listElement",JSON.stringify(element))
+  const handleShopingBag=(el)=>{
+    recent_data.push(el) 
+    localStorage.setItem("listElement",JSON.stringify(recent_data))
     navigate("/ProductList")
   }
 
@@ -94,12 +97,12 @@ let element=JSON.parse(localStorage.getItem("element"))
         <Text color={'red'} margin={'20px'}>{element.discount}</Text>
         <Text margin={'20px'} fontSize={'14px'}>Ratings - {element.rating}*</Text>
         <Select className='option' onChange={handlechange} marginLeft={'20px'} placeholder='Choose Size' >
-        <option value={element.size[0]}>Xtra Small</option>
-        <option value={element.size[1]}>Small</option>
-        <option value={element.size[2]}>Medium</option>
-        <option value={element.size[3]}>Large</option>
-        <option value={element.size[4]}>Xtra Large</option>
-        <option value={element.size[5]}>2*Xtra Large</option>
+        <option value={element.size[0]}>{element.size[0]}</option>
+        <option value={element.size[1]}>{element.size[1]}</option>
+        <option value={element.size[2]}>{element.size[2]}</option>
+        <option value={element.size[3]}>{element.size[3]}</option>
+        <option value={element.size[4]}>{element.size[4]}</option>
+        <option value={element.size[5]}>{element.size[5]}</option>
         </Select>
         <Popover>
         <PopoverTrigger>
@@ -119,8 +122,8 @@ let element=JSON.parse(localStorage.getItem("element"))
             <Text>Successfully added in the bag</Text>
              <Text marginTop={'10px'} fontWeight={'bold'}  >{element.title}</Text>
              <Text marginTop={'10px'}>{element.discounted_price}</Text>
-             <Button marginTop={'10px'} borderRadius={'none'} color={'white'} bgColor={'black'} onClick={handleShopingBag}>VIEW SHOPPING BAG</Button>
-             <Button marginTop={'10px'}>BACK TO SHOP</Button>
+             <Button marginTop={'10px'} borderRadius={'none'} color={'white'} bgColor={'black'} onClick={()=>handleShopingBag(element)}>VIEW SHOPPING BAG</Button>
+             <Button marginTop={'10px'} onClick={()=>setFlag(false)}>BACK TO SHOP</Button>
           </PopoverContent>:""
         }
         </Popover>
@@ -135,7 +138,7 @@ let element=JSON.parse(localStorage.getItem("element"))
              <Text marginTop={'10px'} fontWeight={'bold'}  >{element.title}</Text>
              <Text marginTop={'10px'}>{element.discounted_price}</Text>
              <Button marginTop={'10px'} backgroundColor={'white'} borderRadius={'none'} color={'white'} bgColor={'black'}>VIEW WISHLIST <svg style={{marginLeft:'5px'}} width={'18px'} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg></Button>
-             <Button marginTop={'10px'} >BACK TO SHOP</Button>
+             <Button marginTop={'10px'} onClick={()=>setFlag(false)} >BACK TO SHOP</Button>
           </PopoverContent>:""
         }
         </Popover>
--- project/src/components/ProductList.jsx
+++ project/src/components/ProductList.jsx
@@ -1,12 +1,73 @@
-import React from 'react'
+import { Box, Button, Center, Flex, Heading, Image, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { SimpleGrid, Grid, GridItem } from '@chakra-ui/react'
 
 
-
-let array=JSON.parse(localStorage.getItem('listElement'))||[]
-console.log(array)
 const ProductList = () => {
+  const [quantity,setQuantity]=useState(1)
+  let array=JSON.parse(localStorage.getItem('listElement'))||[]
+
+  const handleDelete=(index)=>
+  {
+    console.log(index)
+    array.splice(index,1)
+    localStorage.setItem('listElement',JSON.stringify(array))
+  }
+ 
+  console.log(array)
   return (
-    <div>ProductList</div>
+    <Center>
+      <Box width='80%' border={'1px solid red'} height={'400px'}>
+       <Flex justifyContent={'space-around'} >
+        <Text>YOUR SHOPPING BAG</Text>
+        <Button borderRadius={'none'} color={'white'} bgColor={'black'}>PROCEED TO CHECKOUT</Button>
+       </Flex>
+       <Box width="100%" border={'1px solid blue'} height='90%'>
+        {/* product list  */}
+       {
+        array.map((el,index)=>(
+          <Box key={el.id} marginTop={'20px'} borderBottom={'1px solid gray'} >
+    <SimpleGrid columns={[1,1,1,2,2,2]} spacing={0} p='8' borderTop='1px' borderColor='grey' w={['95%','95%','95%','100%','100%','100%']}>
+  <Box height={['260px', '260px','260px','180px','180px','180px']} borderTop='1px' borderColor='grey'>
+  <Grid templateColumns={['55% 55%','25% 30%','25% 30%','25% 30%','25% 30%','25% 30%',]} gap={2} mt='4'>
+  <GridItem w='100%' h='180px' >
+    <Image src={el.images[el.images.length-1]} alt='product' h='100%' ></Image>
+  </GridItem>
+  <GridItem w='100%' h='auto' >
+    <Box align='left' mt='6'>{el.title}</Box>
+    <Box align='left'>desc</Box>
+    <Box align='left'>Size:   <span>size</span></Box>
+    <Box align='left'>item no.:   <span>{el.id}</span> </Box>
+    <Button onClick={()=>handleDelete(index)}>X Remove</Button>
+  </GridItem>
+</Grid>
+  </Box>
+  <Box height='auto' align='right'  borderTop='1px' borderColor='grey'>
+     <Box h='120%' w='50%' mt='-8'>
+     <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Price</GridItem>
+  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Quantity</GridItem>
+  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Subtotal</GridItem>
+</Grid>
+<Grid templateColumns='repeat(5, 1fr)' gap={6} >
+  <GridItem w='100%' h='10' align='center' opacity='80%' >{el.discounted_price}</GridItem>
+  <button style={{backgroundColor:'rgb(240, 240, 240)'}}  disabled={quantity==1} onClick={()=>setQuantity((prev)=>prev-1)}>-</button>
+  <GridItem w='100%' h='10' align='center' >{quantity}</GridItem>
+  <button style={{backgroundColor:'rgb(240, 240, 240)'}} onClick={()=>setQuantity((prev)=>prev+1)}>+</button>
+  <GridItem w='100%' h='10' align='center' opacity='80%' >{Number(el.discounted_price)*quantity}</GridItem>
+</Grid>
+     </Box>
+  </Box>
+</SimpleGrid>
+    </Box>
+        ))
+       }
+
+
+
+       </Box>
+    </Box>
+    </Center>
   )
 }
 
