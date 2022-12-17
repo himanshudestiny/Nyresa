2c902e9319b1d1482375745594cd90e6fa3d40ff
Yasin051197 <yasinjamadar0511@gmail.com>
2022-12-18 00:22:51 +0530

    added quantity in json

--- project/src/components/ProductList.jsx
+++ project/src/components/ProductList.jsx
@@ -1,70 +1,103 @@
-import { Box, Button, Center, Flex, Heading, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, Heading, Image, List, Text } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { SimpleGrid, Grid, GridItem } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+import Product from './Confirm/Product'
+import ListChild from './ListChild'
 
+const getData=()=>{
+  return fetch(` http://localhost:8080/productlist`).then((res)=>res.json())
+}
 
 const ProductList = () => {
-  const [quantity,setQuantity]=useState(1)
-  let array=JSON.parse(localStorage.getItem('listElement'))||[]
-
-  const handleDelete=(index)=>
+  
+  const navigate=useNavigate()
+  const [loading,setLoading]=useState(false)
+  const [error,setError]=useState(false)
+  const [product,setProduct]=useState([])
+  let total=0;
+  // let array=JSON.parse(localStorage.getItem('listElement'))||[]
+useEffect(()=>
+{
+  setLoading(true)
+  getData().then((res)=>
   {
-    console.log(index)
-    array.splice(index,1)
-    localStorage.setItem('listElement',JSON.stringify(array))
+    setLoading(false)
+    setError(false)
+    setProduct(res)
+  })
+  .catch((err)=>
+  {
+    setLoading(false)
+    setError(true)
+  })
+},[])
+if(loading)
+{
+  return <div>LOADING........</div>
+}
+if(error)
+{
+  return <div>ERROR</div>
+}
+
+  const handleDelete=async(id)=>
+  {
+    // console.log(index)
+    // array.splice(index,1)
+    // localStorage.setItem('listElement',JSON.stringify(array))
+    let res= await fetch(` http://localhost:8080/productlist/${id}`,
+    {
+      method:'DELETE',
+      headers:{
+        'Content-Type':'application/jaon',
+      },
+      body:JSON.stringify({id})
+    }).then((res)=>res.json())
+
+    getData().then((res)=>
+  {
+    setLoading(false)
+    setError(false)
+    setProduct(res)
+  })
+  .catch((err)=>
+  {
+    setLoading(false)
+    setError(true)
+  })
   }
- 
-  console.log(array)
+  const handleCheckout=()=>
+  {
+    navigate("/delivery")
+  }
   return (
     <Center>
-      <Box width='80%' border={'1px solid red'} height={'400px'}>
-       <Flex justifyContent={'space-around'} >
+      <Box width='80%' height={'400px'}>
+       <div className='checkoutDiv' >
         <Text>YOUR SHOPPING BAG</Text>
-        <Button borderRadius={'none'} color={'white'} bgColor={'black'}>PROCEED TO CHECKOUT</Button>
-       </Flex>
-       <Box width="100%" border={'1px solid blue'} height='90%'>
-        {/* product list  */}
-       {
-        array.map((el,index)=>(
-          <Box key={el.id} marginTop={'20px'} borderBottom={'1px solid gray'} >
-    <SimpleGrid columns={[1,1,1,2,2,2]} spacing={0} p='8' borderTop='1px' borderColor='grey' w={['95%','95%','95%','100%','100%','100%']}>
-  <Box height={['260px', '260px','260px','180px','180px','180px']} borderTop='1px' borderColor='grey'>
-  <Grid templateColumns={['55% 55%','25% 30%','25% 30%','25% 30%','25% 30%','25% 30%',]} gap={2} mt='4'>
-  <GridItem w='100%' h='180px' >
-    <Image src={el.images[el.images.length-1]} alt='product' h='100%' ></Image>
-  </GridItem>
-  <GridItem w='100%' h='auto' >
-    <Box align='left' mt='6'>{el.title}</Box>
-    <Box align='left'>desc</Box>
-    <Box align='left'>Size:   <span>size</span></Box>
-    <Box align='left'>item no.:   <span>{el.id}</span> </Box>
-    <Button onClick={()=>handleDelete(index)}>X Remove</Button>
-  </GridItem>
-</Grid>
-  </Box>
-  <Box height='auto' align='right'  borderTop='1px' borderColor='grey'>
-     <Box h='120%' w='50%' mt='-8'>
-     <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Price</GridItem>
-  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Quantity</GridItem>
-  <GridItem w='100%' h='10' align='center' fontWeight='bolder' >Subtotal</GridItem>
-</Grid>
-<Grid templateColumns='repeat(5, 1fr)' gap={6} >
-  <GridItem w='100%' h='10' align='center' opacity='80%' >{el.discounted_price}</GridItem>
-  <button style={{backgroundColor:'rgb(240, 240, 240)'}}  disabled={quantity==1} onClick={()=>setQuantity((prev)=>prev-1)}>-</button>
-  <GridItem w='100%' h='10' align='center' >{quantity}</GridItem>
-  <button style={{backgroundColor:'rgb(240, 240, 240)'}} onClick={()=>setQuantity((prev)=>prev+1)}>+</button>
-  <GridItem w='100%' h='10' align='center' opacity='80%' >{Number(el.discounted_price)*quantity}</GridItem>
-</Grid>
-     </Box>
-  </Box>
-</SimpleGrid>
-    </Box>
-        ))
-       }
-
-
-
+        <Button className='checkoutDiv_button' borderRadius={'none'} color={'white'} bgColor={'black'} onClick={handleCheckout}>PROCEED TO CHECKOUT</Button>
+       </div>
+       <Box width="100%"  height='auto'>
+        {
+          product.map((el) => (
+            <div style={{borderBottom:'1px solid gray',padding:"1rem"}} key={el.id}>
+              <ListChild key={el.id} title={el.title} id={el.id} desc={el.subtitle} price={el.discounted_price} image={el.images[el.images.length-1]} />
+              <div className='checkoutDiv'>
+              <Button  bgColor={'red'} color={'white'} onClick={()=>handleDelete(el.id)}>X Remove</Button>
+              <Button className='checkoutDiv_button'>Add To Wishlist</Button>
+              </div>
+            </div >
+          ))
+        }
+        <Box>
+          {product.map((el)=>{
+            console.log(total+=Number(el.discounted_price))
+          })}
+          <Text fontWeight={'bold'} align={'right'}>Total:- ${total}</Text>\
+          <br />
+          <Button  className='checkoutDiv_button'  borderRadius={'none'} color={'white'} bgColor={'black'} onClick={handleCheckout}>PROCEED TO CHECKOUT</Button>
+        </Box>
        </Box>
     </Box>
     </Center>
