937e20eee83583f7da8f50a941a31dce515eb81e
zeeshani26 <zeeshan02679@gmail.com>
2022-12-18 03:40:01 +0530

    Final,check for errors

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Flex,
@@ -17,10 +17,49 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 import "./Navbar.css";
+import axios from "axios";
 
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  let [mytext, setMytext] = useState(true);
+  let [cart, setCart] = useState([]);
 
+  let mn = setInterval(() => {
+    setMytext(!mytext);
+
+    clearInterval(mn);
+  }, 5000);
+
+  let abc = () => (
+    <Box>
+      {mytext
+        ? "FREE SHIPPING on orders over $800"
+        : "FREE RETURNS within 30 days"}
+    </Box>
+  );
+
+  const getCart = async () => {
+    axios
+      .get(`http://localhost:8080/productlist`)
+      .then((res) => setCart(res.data));
+  };
+
+  useEffect(() => {
+    abc();
+  }, [mytext]);
+  useEffect(() => {
+    console.log(getCart());
+  }, []);
+  // console.log(cart);
+  // let parent = document.querySelector(".stickyNav").parentElement;
+
+  // while (parent) {
+  //   const hasOverflow = getComputedStyle(parent).overflow;
+  //   if (hasOverflow !== "visible") {
+  //     console.log(hasOverflow, parent);
+  //   }
+  //   parent = parent.parentElement;
+  // }
   return (
     <Box>
       <Box mb={7} paddingBottom="1rem">
@@ -67,14 +106,14 @@ export default function Navbar() {
               <Text cursor="pointer">My account</Text>
             </Link>
 
-            <Link id="heart" to="/wishlist" style={{ display: "flex" }}>
+            <Link id="heart" to="/" style={{ display: "flex" }}>
               <Text cursor="pointer">My wishlist </Text>
               <Box mt={{ base: "1px", md: "2px", lg: "4px" }} pl="3px">
                 <AiOutlineHeart />
               </Box>
             </Link>
             <Link to="/">
-              <Text cursor="pointer">International | English</Text>
+              <Text cursor="pointer">India | English</Text>
             </Link>
           </Flex>
         </Flex>
@@ -198,12 +237,24 @@ export default function Navbar() {
                   fontWeight="400"
                   ml={2}
                 >
-                  {0}
+                  {cart.length}
                 </Text>
               </Box>
             </Flex>
           </Link>
         </Flex>
+      </Box>
+      <Box
+        className="stickyNav"
+        padding={{
+          base: "0px 10px 15px 0px",
+          sm: "0px 80px 15px 0px",
+          lg: "3px 100px 15px 0px",
+          xl: "3px 180px 15px 0px",
+        }}
+        fontSize={{ base: "10px", md: "12px", lg: "14px", xl: "18px" }}
+      >
+        {abc()}
       </Box>
     </Box>
   );
