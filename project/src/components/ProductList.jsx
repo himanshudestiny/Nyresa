0b012d21cecfb28fd4e7a9ee0270272a67919221
Yasin051197 <yasinjamadar0511@gmail.com>
2023-03-25 16:51:52 +0530

    added files

--- project/src/components/ProductList.jsx
+++ project/src/components/ProductList.jsx
@@ -107,11 +107,7 @@ const ProductList = () => {
           <div className="checkoutDiv">
             <Text>YOUR SHOPPING BAG</Text>
             <Button
-              className="checkoutDiv_button"
-              borderRadius={"none"}
-              color={"white"}
-              bgColor={"black"}
-              onClick={handleCheckout}
+              
             >
               PROCEED TO CHECKOUT
             </Button>
